153259c83a658b965c65387b823eeb383f0eeff7
Wasin Wongkum <kkmonster@hotmail.com>
2017-10-24 22:03:02 +0700

    add  commit

--- blink5/blink5.ino
+++ blink5/blink5.ino
@@ -1,10 +1,21 @@
 void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
-  pinMode(14, INPUT); // Pao
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+
+
+
+
+
+
+
+
+
+//  haha
 
 }
